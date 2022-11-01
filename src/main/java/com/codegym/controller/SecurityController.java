686311bc5168d059f5f105ffa70fb98ebe5e1307
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-11-01 16:01:15 +0700

    welcome admin

--- src/main/java/com/codegym/controller/SecurityController.java
+++ src/main/java/com/codegym/controller/SecurityController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -26,5 +27,11 @@ public class SecurityController {
     public String Homepage(Model model) {
         model.addAttribute("user", getPrincipal());
         return "/welcome";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage(ModelMap modelMap) {
+        modelMap.addAttribute("user", getPrincipal());
+        return "/admin";
     }
 }
