8fec05e532ba45987b37687be209477546be1e0d
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-11-01 15:58:24 +0700

    welcome user

---
+++ src/main/java/com/codegym/configuration/AppInitializer.java
@@ -0,0 +1,22 @@
+package com.codegym.configuration;
+
+import com.codegym.security.SecurityConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
